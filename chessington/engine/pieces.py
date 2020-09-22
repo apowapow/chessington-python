d13c58dbbedaf0d86b5f8a22baaf5781a560ebe1
POWER Alexander <71255126+apowapow@users.noreply.github.com>
2020-09-22 12:58:26 +0100

    refactor: maybe_add_square
    an empty piece is allowed providing that it is not a takeable piece; a non-empty piece is allowed providing that it is a takeable piece

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -20,15 +20,17 @@ class Piece(ABC):
         self.player = player
         self.moved = False
 
-    def check_empty_square(self, square, board):
-        return board.get_piece(square) is None
+    def maybe_add_square(self, squarelist, square, board, takeable):
+        if 0 <= square.row < BOARD_SIZE and 0 <= square.col < BOARD_SIZE:
+            piece = board.get_piece(square)
 
-    def within_boundaries(self, square):
-        return 0 <= square.row < BOARD_SIZE and 0 <= square.col < BOARD_SIZE
+            if piece is None:
+                if not takeable:
+                    squarelist.append(square)
 
-    def has_opposite_colour(self, square, board):
-        piece = board.get_piece(square)
-        return piece is not None and piece.player != self.player
+            elif piece.player != self.player:
+                if takeable:
+                    squarelist.append(square)
 
     @abstractmethod
     def get_available_moves(self, board):
@@ -57,26 +59,31 @@ class Pawn(Piece):
         pos = board.find_piece(self)
 
         # in front
-        sq_one = Square.at(pos.row + 1 if white else pos.row - 1, pos.col)
+        self.maybe_add_square(
+            squarelist=moves,
+            square=Square.at(pos.row + 1 if white else pos.row - 1, pos.col),
+            board=board,
+            takeable=False)
 
-        if self.within_boundaries(sq_one) and self.check_empty_square(sq_one, board):
-            moves.append(sq_one)
-
-            if not self.moved:
-                sq_two = Square.at(pos.row + 2 if white else pos.row - 2, pos.col)
-
-                if self.within_boundaries(sq_one) and self.check_empty_square(sq_two, board):
-                    moves.append(sq_two)
+        if len(moves) == 1 and (not self.moved):
+            self.maybe_add_square(
+                squarelist=moves,
+                square=Square.at(pos.row + 2 if white else pos.row - 2, pos.col),
+                board=board,
+                takeable=False)
 
         # diagonal
-        sq_left = Square.at(pos.row + 1 if white else pos.row - 1, pos.col - 1)
-        sq_right = Square.at(pos.row + 1 if white else pos.row - 1, pos.col + 1)
+        self.maybe_add_square(
+            squarelist=moves,
+            square=Square.at(pos.row + 1 if white else pos.row - 1, pos.col - 1),
+            board=board,
+            takeable=True)
 
-        if self.within_boundaries(sq_left) and self.has_opposite_colour(sq_left, board):
-            moves.append(sq_left)
-
-        if self.within_boundaries(sq_right) and self.has_opposite_colour(sq_right, board):
-            moves.append(sq_right)
+        self.maybe_add_square(
+            squarelist=moves,
+            square=Square.at(pos.row + 1 if white else pos.row - 1, pos.col + 1),
+            board=board,
+            takeable=True)
 
         return moves
 
